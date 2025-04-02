28a11b21d7d438061ea5a6af9d0e9200be3b8eb3
erosnoxx <garotoslixo@gmail.com>
2025-04-01 23:08:39 -0300

    first commit

---
+++ src/main/java/com/erosnox/imobli/ImobliApplication.java
@@ -0,0 +1,13 @@
+package com.erosnox.imobli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImobliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImobliApplication.class, args);
+	}
+
+}
